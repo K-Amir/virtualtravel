82065cd6986e4b3c03f873d993318a3a30499b4d
Khalilov <skhlamir@gmail.com>
2022-04-29 14:57:32 +0200

    Added testing

---
+++ BackWeb/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class SampleTest {
+
+
+    @Test
+    public void sampleTesting(){
+        Assertions.assertTrue(true);
+    }
+
+}
